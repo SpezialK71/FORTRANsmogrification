021971f54f5878809027130378d6cf2f158057c1
Kurt <130383415+SpezialK71@users.noreply.github.com>
2023-07-15 23:49:29 -0500

    Add files via upload

---
+++ io.f95
@@ -0,0 +1,9 @@
+program io
+    implicit none
+
+    real :: x,y,z
+    print *, 'enter values x, y, and z'
+    read *, x,y,z
+    print *, 'the values you typed for x,y,z are: ', x,y,z
+    
+end program io
